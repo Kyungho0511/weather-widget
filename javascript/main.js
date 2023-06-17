270e15e02188daddfd741414fa33090fe1c6a6f8
Kyungho0511 <kleearchitecture@gmail.com>
2023-06-17 10:00:19 +0900

    Data and APIs # Create main.js
    Interactive Web Submission # Fetch weather data from 'Open Weather Map API'

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,9 @@
-const API_KEY = "";
+const WEATHER_API_KEY = "41281ca44d380f57a549ca6a291b8b59";
+
+function getWeatherData() {
+  fetch(`http://api.openweathermap.org/data/2.5/weather?zip=10128&APPID=${WEATHER_API_KEY}`)
+    .then(response => response.json())
+    .then(data => console.log(data));
+}
+
+getWeatherData();
