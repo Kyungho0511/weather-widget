3a917ffe8df52fdd3daf70ebfb92286882082855
Kyungho0511 <kleearchitecture@gmail.com>
2023-06-17 12:04:28 +0900

    DOM Manipulation # Update main.js, index.html. style.css
    Interactive Web submission # Update city name, temperature, weather icon on submit zip code

--- javascript/main.js
+++ javascript/main.js
@@ -1,23 +1,37 @@
 const btn = document.querySelector('.search-button');
 const input = document.querySelector('.zipcode');
+const form = document.querySelector('form');
+const image = document.querySelector('img');
+const CITY_NAME = document.querySelector('.city_name');
+const CITY_TEMP = document.querySelector('.temperature');
 
 const getWeatherData = (zip) => {
-  const WEATHER_API_KEY = "41281ca44d380f57a549ca6a291b8b59";
-  const API_ENDPOINT = `http://api.openweathermap.org/data/2.5/weather?zip=${zip}&APPID=${WEATHER_API_KEY}`;
+  const API_KEY = "41281ca44d380f57a549ca6a291b8b59";
+  const API_ENDPOINT = `http://api.openweathermap.org/data/2.5/weather?zip=${zip}&APPID=${API_KEY}`;
 
   fetch(API_ENDPOINT)
     .then(response => response.json())
     .then(data => {
-      let local_weather_data = data;
-      console.log(local_weather_data);
-    });
+      const local_weather_data = data;
+      const weather_in_celsius = Math.round(local_weather_data.main.temp - 273);
+      const weather_icon = local_weather_data.weather[0].icon;
+      console.log(weather_icon);
+      CITY_NAME.textContent = local_weather_data.name;
+      CITY_TEMP.textContent = `${weather_in_celsius} C`;
+      image.setAttribute('src', `https://openweathermap.org/img/wn/${weather_icon}@2x.png`);
+    })
+    .catch(error => alert(`${error}`));
+
+  form.reset();
+  input.focus();
 }
 
 const getZipcode = (event) => {
   event.preventDefault();
-  let ZIP_CODE = input.value;
+  const ZIP_CODE = input.value;
   getWeatherData(ZIP_CODE);
 }
 
-btn.addEventListener('click', event => getZipcode(event));
+// btn.addEventListener('click', event => getZipcode(event));
+form.addEventListener('submit', event => getZipcode(event));
 
